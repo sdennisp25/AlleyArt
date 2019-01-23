09abf7a71772738f23084a152b08c3a49ac25300
Cat <madcatmac16@gmail.com>
2019-01-23 00:41:06 -0700

    nav css

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -39,7 +39,7 @@ class Nav extends Component {
                         {this.props.user.isArtist === true && <a href="/image">Upload New Art</a>}
                         
 						
-						<a href="/" onClick={this.burgerToggle}><i className="fas fa-sign-out-alt">Sign out</i></a>
+						<a href="/" onClick={this.burgerToggle}><i className="sign-out">Sign out</i></a>
 					
 
                     </div>
