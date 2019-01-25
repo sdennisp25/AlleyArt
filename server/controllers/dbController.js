5519b53b15c62efce830d10909d6125aecf554bd
CAM <34009133+CMichael82@users.noreply.github.com>
2019-01-24 20:29:30 -0700

    removed some old comments

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -128,31 +128,4 @@ module.exports = {
 				res.json(err);
 			})
 	}
-
-	/////////////////OTHER FUNCTIONS - MAY STILL NEED/////////////////
-	// findById: function (req, res) {
-	// 	db.Artwork
-	// 		.findById(req.params.id)
-	// 		.then(art => res.json(art))
-	// 		.catch(err => res.status(422).json(err));
-	// },
-	// create: function (req, res) {
-	// 	db.Artwork
-	// 		.create(req.body)
-	// 		.then(art => res.json(art))
-	// 		.catch(err => res.status(422).json(err));
-	// },
-	// update: function (req, res) {
-	// 	db.Artwork
-	// 		.findOneAndUpdate({ _id: req.params.id }, req.body)
-	// 		.then(art => res.json(art))
-	// 		.catch(err => res.status(422).json(err));
-	// },
-	// remove: function (req, res) {
-	// 	db.Artwork
-	// 		.findById({ _id: req.params.id })
-	// 		.then(art => art.remove())
-	// 		.then(art => res.json(art))
-	// 		.catch(err => res.status(422).json(err));
-	// }
 };
